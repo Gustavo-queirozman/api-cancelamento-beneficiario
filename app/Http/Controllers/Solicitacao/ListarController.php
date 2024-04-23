a986d8df7fc6d67acaae0da3617ac6edade92cd5
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-04-23 16:37:49 -0300

    Criado controller para listar dependentes

--- app/Http/Controllers/Solicitacao/ListarController.php
+++ app/Http/Controllers/Solicitacao/ListarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Solicitacao;
 
 use App\Http\Controllers\Controller;
+use App\Models\Beneficiario;
 use Illuminate\Http\Request;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -10,8 +11,13 @@ class ListarController
 {
     use AsAction;
 
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-            
+        $beneficiarios = new Beneficiario();
+        $dependentes = $beneficiarios->selectDependentes($request->input('codigoCarteirinha'));
+
+        return response()->json([
+            'dependentes'=> $dependentes
+        ]);
     }
 }
