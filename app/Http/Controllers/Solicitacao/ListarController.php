7baf437ababaf1654c85f10866e92b63c21128f1
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-04-23 16:25:23 -0300

    Criado controller para listar beneficiario durante solicitacao

---
+++ app/Http/Controllers/Solicitacao/ListarController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Solicitacao;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ListarController
+{
+    use AsAction;
+
+    public function __invoke()
+    {
+              
+    }
+}
